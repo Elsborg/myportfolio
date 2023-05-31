df57b8c40d83a7bf5e1ec26d856a98f7e905e1f4
elsborg <martin.elsborg@gmail.com>
2023-05-31 11:37:52 +0200

    Intitial commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        title: 'Portfolio Website',
+        imgUrl: 'https://fastly.picsum.photos/id/72/600/400.jpg?hmac=kDSCE-9TrmDVBF6QmxfzQeNvLa0DSzSN4fknMFt6pkc',
+        stack: ['Tailwind', 'Vite', 'React'],
+        link: 'https://google.com',
+    },
+    {
+        title: 'Course status',
+        imgUrl: 'https://fastly.picsum.photos/id/955/600/400.jpg?hmac=80PvxPd6BLKqVO9Vll0pulScP3lwkE4JI943jE-TuTw',
+        stack: ['React Native', 'GraphQL', 'JavaScript'],
+        link: 'https://google.com',
+    },
+    {
+        title: 'Pin Positions',
+        imgUrl: 'https://fastly.picsum.photos/id/631/600/400.jpg?hmac=rYjpfAImhk4GM_Il-sn44bgx4DmZZCRSmdijkCkSpuQ',
+        stack: ['React Native', 'GraphQL', 'JavaScript'],
+        link: 'https://google.com',
+    },
+]
+
---
+++ src/data/timeline.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        year: '2023',
+        title: 'AP Graduate in Multimediadesign',
+        duration: '2 years',
+        details: "As a front-end developer, my AP Graduate in Multimedia Design has been invaluable. I've honed my skills in Graphic Design, Web Design, and Interactive Design, becoming proficient with Adobe Suite, HTML/CSS, and JavaScript. The degree emphasized UI/UX principles, enhancing my ability to create engaging user interfaces. Additionally, my knowledge of Animation & Video Production brings an extra level of dynamism to my work.",
+
+    },
+    {
+        year: '2023',
+        title: 'Internship at GLFR by Ingenium Golf',
+        duration: '6 months',
+        details: "At my internship with GLFR, I crafted a new user interface for greenkeepers, focusing on usability and streamlined workflows. The last 2.5 month, I wrote my final paper, further solidifying my understanding of user-centered design. This experience enhanced my practical insights into real-world design scenarios."
+    },
+    {
+        year: '2023',
+        title: 'Service employee - Meny',
+        duration: '8 years',
+        details: "Having previously worked as a service employee in a retail store, I gained significant experience in customer interaction and store operations. I managed daily transactions, customer queries, product recommendations, and maintained store cleanliness. While I no longer work in this role, the skills I developed during my tenure—particularly in customer service and organization—have enriched my professional competencies and continue to serve me in my career progression.",
+    },
+    {
+        year: '2019',
+        title: 'Bachelor in Software Engineering',
+        duration: '2 years',
+        details: "After considerable reflection and analysis of my personal and career objectives, I made the difficult decision to leave my bachelor degree in software engineering. I feel confident that this decision aligns with my current goals and the direction I want my professional path to take. Despite not completing the program, the experience has provided me valuable insights and skills that I can leverage in the future."
+    },
+]
