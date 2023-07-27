c68d466c9ece3594a392782bf7e031961729824e
elsborg <martin.elsborg@gmail.com>
2023-07-27 15:11:11 +0200

    responsive intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import image from '/assets/portfolioImage.jpeg'
 
 const Intro = () => {
     return (
-        <div className='flex items-center flex-row justify-around pt-20 pb-6'>
+        <div className='flex items-center flex-col md:flex-row justify-around pt-20 pb-6'>
             <div className='flex flex-col'>
             <h1 className='text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold'>Martin</h1>
             <p className='text-base md_text-xl mb-3 font-medium'>Full Stack Developer</p>
