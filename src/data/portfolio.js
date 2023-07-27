f327960129ab7ea16f7c45dbc697694d0abaaaea
elsborg <martin.elsborg@gmail.com>
2023-07-27 14:55:42 +0200

    deploy again

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: 'Portfolio Website',
-        imgUrl: '/assets/site.png',
+        imgUrl: './assets/site.png',
         stack: ['Tailwind', 'Vite', 'React'],
         link: 'https://google.com',
     },
     {
         title: 'Phonebook',
-        imgUrl: '/assets/phonebook.png',
+        imgUrl: './assets/phonebook.png',
         stack: ['React', 'Node', 'JavaScript', 'Express'],
         link: 'https://person-service.onrender.com/',
     },
     {
         title: 'Golf pin position interface',
-        imgUrl: '/assets/mockupgreenkeeper.jpg',
+        imgUrl: './assets/mockupgreenkeeper.jpg',
         stack: ['React Native', 'GraphQL', 'JavaScript'],
         link: 'https://www.linkedin.com/feed/update/urn:li:activity:7071808889163259904/',
     },
